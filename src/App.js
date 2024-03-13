f8506a084c847431394976ab96049199a6f7ad2c
stani1slav1 <1373081@ukr.net>
2024-03-13 19:46:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from "./data";
 import './App.css';
 
 function App() {
+
+  const [cottage, setCottage] = useState(0);
+  const { id, name, price, email, image } = data[cottage];
+  
+  const previousCottage = () => {
+    setCottage((cottage => {
+      cottage --;
+      if(cottage < 0) {
+        cottage = data.length -1;
+      }
+      return cottage;
+    }))
+  }
+
+  const nextCottage = () => {
+    setCottage((cottage => {
+      cottage ++;
+      if(cottage > data.length -1) {
+        cottage = 0;
+      }
+      return cottage;
+    }))
+  }
+
   return (
+    <div className='color'>
+
+    <div className="App ppa">
+      <img src={image} width={500} height={350} alt='picture'/>
+    </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{id} - {name}</h1>
+    </div>
+    <div className="App">
+      <h2>{price}</h2>
+    </div>
+    <div className="App">
+      <h2>{email}</h2>
+    </div>
+    <div className='App'>
+      <button onClick={previousCottage}><img src='https://cdn.icon-icons.com/icons2/4091/PNG/96/arrow_left_icon_259686.png'/></button>
+      <button onClick={nextCottage}><img src='https://cdn.icon-icons.com/icons2/4091/PNG/96/arrow_right_icon_259656.png'/></button>
+    </div>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        name: "Коттедж в Циркунах",
+        price: "110 000 $",
+        email: "info@dekostroy.com",
+        image: "https://dekostroy.com.ua/images/prodaja-kottedjey-kharkiv/dvuhetagniy-kottedg-kupit-kharkov-deko.jpg"
+    },
+    {
+        id: 2,
+        name: "Коттедж WhiteWood",
+        price: "100 000 $",
+        email: "novobudovy.com@gmail.com",
+        image: "https://novobudovy.com/images/wm/78046b40e43a80f7fc9fa3098022030f.png"
+    },
+    {
+        id: 3,
+        name: "Коттеджный городок Park House",
+        price: "260 000 $",
+        email: "lesopark.kharkov.ua",
+        image: "https://novobudovy.com/images/wm/9fe732cac751e5e29f670531cbe0e9a5.png" 
+    },
+    {
+        id: 4,
+        name: "Коттеджный городок River Park",
+        price: "124 120 $",
+        email: "novobudovy.com@gmail.com",
+        image: "https://novobudovy.com/images/wm/247a5a9fc0920e35d2e763f379455826.png" 
+    },
+    {
+        id: 5,
+        name: "Коттеджный городок Artvillage",
+        price: "68 000 $",
+        email: "novobudovy.com@gmail.com",
+        image: "https://novobudovy.com/images/wm/6bd42ac2edfe7edbaa38b71a686743df.png" 
+    }
+]
